e4ae8380509582dbf80f073370bb3ebec465ce2d
NMartynova <nataliia.prokopenko@medeanalytics.com>
2020-10-06 12:26:47 +0300

    HW_Task#2

--- src/com/company/Ex97.java
+++ src/com/company/Ex97.java
@@ -1,15 +1,8 @@
 package com.company;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.Random;
-import java.util.Scanner;
-
 public class Ex97 {
 
     public static void main(String[] args) {
-
-
         //int sizeOfArray = new Scanner(System.in).nextInt();
         int sizeOfArray = 6;
         System.out.println("n-размерность матрицы " + sizeOfArray);
@@ -17,15 +10,12 @@ public class Ex97 {
         int[][] arrayConverted = new int[sizeOfArray][sizeOfArray];
         int zeroColumn = Math.abs((int) (Math.random() * ((sizeOfArray - 1) + (sizeOfArray - 1))) - (sizeOfArray - 1));
         int zeroLine = Math.abs((int) (Math.random() * ((sizeOfArray - 1) + (sizeOfArray - 1))) - (sizeOfArray - 1));
-
         System.out.println("n-размерность матрицы");
         //int sizeOfArray = new Scanner(System.in).nextInt();
-
          /*9.Ввести с консоли n-размерность матрицы a [n] [n]. Задать значения элемен-
     тов матрицы в интервале значений от -n до n с помощью датчика случайных чи-
     сел*/
         // толбцы и стороки запонены нулями, номера их генеряться случайно
-
         System.out.println("Матрица: ");
         for (int i = 0; i < sizeOfArray; i++) {
             for (int j = 0; j < sizeOfArray; j++) {
@@ -40,9 +30,7 @@ public class Ex97 {
             }
             System.out.println("");
         }
-
 //9.7. Уплотнить матрицу, удаляя из нее строки и столбцы, заполненные нулями.
-
         // поиск нулевой строки
         System.out.println("матрицу, удаляя из нее строки и столбцы, заполненные нулями");
         boolean[] arrayLine = new boolean[sizeOfArray];
@@ -76,36 +64,30 @@ public class Ex97 {
             if (!arrayLine[i]) {
                 zeroLineIndex[0] = i;
             }
-            if (!arrayColumn[i])
-            {
+            if (!arrayColumn[i]) {
                 zeroColumnIndex[0] = i;
             }
         }
-
         // новая матрица
-        int [][] arrayWithoutZeroLine = new int[sizeOfArray-1][sizeOfArray];
-        for (int i = 0; i < sizeOfArray-1; i++) {
+        int[][] arrayWithoutZeroLine = new int[sizeOfArray - 1][sizeOfArray];
+        for (int i = 0; i < sizeOfArray - 1; i++) {
             for (int j = 0; j < sizeOfArray; j++) {
-                if (i >= zeroLineIndex[0])
-                {
-                    arrayWithoutZeroLine[i][j] = arrayInitial[i+1][j];
+                if (i >= zeroLineIndex[0]) {
+                    arrayWithoutZeroLine[i][j] = arrayInitial[i + 1][j];
                 }
-                if (i < zeroLineIndex[0])
-                {
+                if (i < zeroLineIndex[0]) {
                     arrayWithoutZeroLine[i][j] = arrayInitial[i][j];
                 }
             }
         }
         System.out.println("");
-        int [][] arrayWithoutZeroLineAndColumn = new int[sizeOfArray-1][sizeOfArray];
-        for (int i = 0; i < sizeOfArray-1; i++) {
-            for (int j = 0; j < sizeOfArray-1; j++) {
-                if (j >= zeroColumnIndex[0])
-                {
-                    arrayWithoutZeroLineAndColumn[i][j] = arrayWithoutZeroLine[i][j+1];
+        int[][] arrayWithoutZeroLineAndColumn = new int[sizeOfArray - 1][sizeOfArray];
+        for (int i = 0; i < sizeOfArray - 1; i++) {
+            for (int j = 0; j < sizeOfArray - 1; j++) {
+                if (j >= zeroColumnIndex[0]) {
+                    arrayWithoutZeroLineAndColumn[i][j] = arrayWithoutZeroLine[i][j + 1];
                 }
-                if (j < zeroColumnIndex[0])
-                {
+                if (j < zeroColumnIndex[0]) {
                     arrayWithoutZeroLineAndColumn[i][j] = arrayWithoutZeroLine[i][j];
                 }
                 System.out.print(arrayWithoutZeroLineAndColumn[i][j] + " ");
